092f54a749a70ee0b52e86357fda30de386e2ee3
mario-allemann <allemann2@hotmail.com>
2017-10-31 19:25:42 +0100

    chatLab: completed send & receive methods using serialization

--- chatLab/messages/Message.java
+++ chatLab/messages/Message.java
@@ -1,9 +1,13 @@
 package chatLab.messages;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.Socket;
 import com.sun.istack.internal.logging.Logger;
 
-public class Message {
+public class Message implements Serializable{
 	private static Logger logger = Logger.getLogger("", null);
 	protected MessageType type;
 	
@@ -13,10 +17,28 @@ public class Message {
 	
 	public void send(Socket socket) {
 		logger.info("Sending message");
+		
+		
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			out.writeObject(this);
+			out.flush();
+			socket.shutdownOutput();
+			
+		} catch(Exception e) {
+			logger.warning(e.toString());
+		}
 	}
 	
 	public static Message receive(Socket socket) {
 		logger.info("Receiving Message");
+		try {
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			return (Message) in.readObject();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			logger.warning(e.toString());
+		}
 		return null;
 	}
 	
